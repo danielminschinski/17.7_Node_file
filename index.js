e973d3d4a805b650f4e5f2edd14e9d243cde963d
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-19 14:14:05 +0200

    Update index.js and add dirFiles.txt

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var fs = require('fs');
+var colors = require('colors');
+var StatMode = require('stat-mode');
 
-
-fs.readFile('./tekst.txt', 'utf-8', function(err, data){
-	console.log(data);
-});
-
-fs.writeFile('./tekst.txt', 'Tekst, który zapiszemy w pliku tekst.txt', function(err){
+fs.readdir('.', 'utf-8', function(err, files){
+	console.log(files);
 	if(err) throw err;
-	console.log('Zapisano!');	
+
+	fs.writeFile('dirFiles.txt', files, function(err, data){
+		console.log('Pliki zostały zapisane!'.green);
+	});
 });
 
+
+/*fs.readFile('./tekst.txt', 'utf-8', function(err, data){
+	console.log('Dane przed zapisem!' .blue);
+	console.log(data);
+	fs.appendFile('./tekst.txt', '\nA tak wygladaja po zapisie!', function(err){
+		if(err) throw err;
+		console.log('Zapisano!' .blue);	
+		fs.readFile('./tekst.txt', 'utf-8', function(err, data){
+			console.log('Dane po zapisie' .blue)
+			console.log(data);
+		});
+	});
+});*/
